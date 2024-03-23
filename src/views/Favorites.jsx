33e22075e44ee95de0ba683a6139e10c2d8bf7c2
mbrionesz <matias.briones95@gmail.com>
2024-03-23 18:44:33 -0300

    Mensaje de que no se han seleccionado fotos favs

--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,23 +1,27 @@
-import React, { useContext} from "react";
-import MyContext from "../my_context";
+  import React, { useContext} from "react";
+  import MyContext from "../my_context";
 
-const Favorites = () => {
-  const { photos, favorites } = useContext(MyContext)
+  const Favorites = () => {
+    const { photos, favorites } = useContext(MyContext)
 
-  //filtro de fotos
-  const favoritePhotos = photos.filter((photo,index) => favorites[index])
+    //filtro de fotos
+    const favoritePhotos = photos.filter((photo,index) => favorites[index])
 
-  return (
-    <div>
+    return (
+      <div>
       <h1>Fotos favoritas</h1>
       <div className="p-3 gallery grid-columns-4">
-        {favoritePhotos.map((photo) => (
-          <div key={photo.id}>
-            <img src={photo.src.tiny} alt={photo.alt} />
-          </div>
-        ))}
+        {favoritePhotos.length > 0 ? (
+          favoritePhotos.map((photo) => (
+            <div key={photo.id}>
+              <img src={photo.src.tiny} alt={photo.alt} />
+            </div>
+          ))
+        ) : (
+          <p>No se han seleccionado fotos favoritas.</p>  //Cuando se ejecuta la app y al no seleccionar ninguna foto aparece este mensaje
+        )}
       </div>
     </div>
-  );
-};
-export default Favorites;
+    );
+  };
+  export default Favorites;
